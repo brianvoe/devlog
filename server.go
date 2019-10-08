a0649886efc5c798b9e3f1d9d7c9106481ce057e
bvoelker <bvoelker@fluidpay.com>
2019-10-08 17:28:27 -0500

    work in progress

--- server.go
+++ server.go
@@ -31,7 +31,27 @@ func GetHTML(w http.ResponseWriter, r *http.Request) {
 
 // GetData will get the current data set and return it back
 func GetData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Get body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	// Json unmarshal to data
+	d := struct {
+		Level string `json:"level"`
+	}{}
+	if err := json.Unmarshal(body, &d); err != nil {
+		http.Error(w, "Error unmarshalling data", http.StatusInternalServerError)
+		return
+	}
 
 	out := []Data{}
 	DataMap.Range(func(key interface{}, value interface{}) bool {
@@ -44,6 +64,7 @@ func GetData(w http.ResponseWriter, r *http.Request) {
 		return out[i].CreatedAt > out[j].CreatedAt
 	})
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(out)
 }
